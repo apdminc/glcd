8c89b08d2d0ecf72e4c408fd50ad37092b570596
Dave Camarillo <dave.camarillo@gmail.com>
2014-03-11 12:20:52 -0700

    renamed config variable

--- devices/STM32F4_ChibiOS.c
+++ devices/STM32F4_ChibiOS.c
@@ -23,11 +23,11 @@ void glcd_spi_write_multibyte(const uint16_t length, const uint8_t *source_buffe
   }
 
 #else
-  extern const SPIConfig lcd_spi_cfg_new;
+  extern const SPIConfig lcd_spi_cfg;
   //spiStart(CHIBIOS_SPI_PEREPHERIAL, &lcd_spi_cfg);
   //spiSend(CHIBIOS_SPI_PEREPHERIAL, length, source_buffer);//chibios function call
 
-  spiTxOnlyStart(CHIBIOS_SPI_PEREPHERIAL, &lcd_spi_cfg_new);
+  spiTxOnlyStart(CHIBIOS_SPI_PEREPHERIAL, &lcd_spi_cfg);
   spiTxOnlySend(CHIBIOS_SPI_PEREPHERIAL, length, source_buffer);//chibios function call
 #endif
 }
