46d32dc97484f96d674c971e1056964f81028259
Dave Camarillo <dave.camarillo@gmail.com>
2014-03-11 12:23:58 -0700

    purged the bus locking code

--- devices/STM32F4_ChibiOS.h
+++ devices/STM32F4_ChibiOS.h
@@ -4,7 +4,6 @@
 #include "ch.h"
 #include "hal.h"
 #include "spi_tx_only.h"
-#include "bus_locking.h"
 
 #if !defined(GLCD_USE_PARALLEL)
 #if GLCD_USE_SPI_UART
@@ -17,12 +16,8 @@
     #define GLCD_SELECT()       spi_uart_clear_chip_select()
     #define GLCD_DESELECT()     spi_uart_set_chip_select()
 #  else
-    //extern int bus_lock_spi_mtx(const SPIDriver *driver_address);
-    //extern int bus_unlock_spi_mtx(const SPIDriver *driver_address);
-    //#define GLCD_SELECT()     bus_unlock_spi_mtx(CHIBIOS_SPI_PEREPHERIAL); spiSelect(CHIBIOS_SPI_PEREPHERIAL)
-    //#define GLCD_DESELECT()   bus_lock_spi_mtx(CHIBIOS_SPI_PEREPHERIAL); spiUnselect(CHIBIOS_SPI_PEREPHERIAL)
-    #define GLCD_SELECT()     bus_unlock_spi_mtx(CHIBIOS_SPI_PEREPHERIAL); spiSelect(CHIBIOS_SPI_PEREPHERIAL)
-    #define GLCD_DESELECT()   bus_lock_spi_mtx(CHIBIOS_SPI_PEREPHERIAL); spiUnselect(CHIBIOS_SPI_PEREPHERIAL)
+    #define GLCD_SELECT()     spiSelect(CHIBIOS_SPI_PEREPHERIAL)
+    #define GLCD_DESELECT()   spiUnselect(CHIBIOS_SPI_PEREPHERIAL)
 #  endif
 #else
 #   error "Parallel not yet supported under chibios"
